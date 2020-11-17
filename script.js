21b3b77a033627fb4f547cbc6616d5b0190076b6
Pavel680 <74411897+Pavel680@users.noreply.github.com>
2020-11-17 14:59:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ window.onload = function () {
   function CreateNewTask(text) {
 	let newTaskField = document.createElement('form');
 	let newTask = document.createElement('span');
-		newTask.textContent = text;
-		newTask.setAttribute('contenteditable', 'true');
-		out.appendChild(newTaskField);
+	newTask.textContent = text;
+	newTask.setAttribute('contenteditable', 'true');
+	out.appendChild(newTaskField);
     let deleteButton = document.createElement('button');
-		deleteButton.textContent = 'X';
-		deleteButton.className = 'deleteButton';
-		deleteButton.onclick = function () {
-		out.removeChild(newTaskField);
+	deleteButton.textContent = 'X';
+	deleteButton.className = 'deleteButton';
+	deleteButton.onclick = function () {
+	out.removeChild(newTaskField);
     }
     let checkbox = document.createElement('input');
-		checkbox.setAttribute('type', 'checkbox', 'value', 'text');
-		checkbox.className = 'inputCheckbox';
+	checkbox.setAttribute('type', 'checkbox', 'value', 'text');
+	checkbox.className = 'inputCheckbox';
 	newTaskField.appendChild(checkbox);
 	newTaskField.appendChild(newTask);
 	newTaskField.appendChild(deleteButton);
@@ -24,24 +24,22 @@ window.onload = function () {
 	input.onkeyup = function (e) {
 		if( e.keyCode === 13 ) {
 			if ( input.value == input.value.replace( /[^\s]/g, '' ) ) {
-				input.value = '';
-				return null;
+			input.value = '';
+			return null;
 			}
-				CreateNewTask( input.value );
-				input.value = '';
+		CreateNewTask( input.value );
+		input.value = '';
 		} else {
-			return false;
+		return false;
 		}
 	}
 	btn.onclick = function () {
 		if ( input.value == input.value.replace( /[^\s]/g, '' ) ) {
-			
-			input.value = '';
-			return null;	
-			
+		input.value = '';
+		return null;	
 		} else {
-			CreateNewTask( input.value );
-			input.value = '';
+		CreateNewTask( input.value );
+		input.value = '';
 		}
 	}
 
