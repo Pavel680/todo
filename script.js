21b3fb0ee714443f557e00afdb935dda6d7c8db8
Pavel680 <74411897+Pavel680@users.noreply.github.com>
2020-11-17 21:36:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,43 +4,43 @@ window.onload = function () {
 	let btn = document.getElementById('add');
   function CreateNewTask(text) {
 	let newTaskField = document.createElement('form');
-	let newTask = document.createElement('span');
-	newTask.textContent = text;
-	newTask.setAttribute('contenteditable', 'true');
-	out.appendChild(newTaskField);
-	let deleteButton = document.createElement('button');
-	deleteButton.textContent = 'X';
-	deleteButton.className = 'deleteButton';
-	deleteButton.onclick = function () {
-	out.removeChild(newTaskField);
-	}
-	let checkbox = document.createElement('input');
-	checkbox.setAttribute('type', 'checkbox', 'value', 'text');
-	checkbox.className = 'inputCheckbox';
+		let newTask = document.createElement('span');
+			newTask.textContent = text;
+			newTask.setAttribute('contenteditable', 'true');
+			out.appendChild(newTaskField);
+		let deleteButton = document.createElement('button');
+			deleteButton.textContent = 'X';
+			deleteButton.className = 'deleteButton';
+			deleteButton.onclick = function () {
+			out.removeChild(newTaskField);
+			}
+		let checkbox = document.createElement('input');
+			checkbox.setAttribute('type', 'checkbox', 'value', 'text');
+			checkbox.className = 'inputCheckbox';
 	newTaskField.appendChild(checkbox);
 	newTaskField.appendChild(newTask);
 	newTaskField.appendChild(deleteButton);
   }
 input.onkeyup = function (e) {
 	if( e.keyCode === 13 ) {
-	if ( input.value == input.value.replace( /[^\s]/g, '' ) ) {
-	input.value = '';
-	return null;
-	}
+		if ( input.value == input.value.replace( /[^\s]/g, '' ) ) {
+			input.value = '';
+			return null;
+		}
 	CreateNewTask( input.value );
 	input.value = '';
 	} else {
-	return false;
-	}
-	}
+		return false;
+		}
+}
 btn.onclick = function () {
 	if ( input.value == input.value.replace( /[^\s]/g, '' ) ) {
-	input.value = '';
-	return null;	
+		input.value = '';
+		return null;	
 	} else {
-	CreateNewTask( input.value );
-	input.value = '';
-	}
+		CreateNewTask( input.value );
+		input.value = '';
+		}
 	}
 
 	
